6357a8f4d9a8fccb0f2a4955074dd6a1377770d6
Brandon Alexis <brandonmachado159@gmail.com>
2022-05-06 20:22:34 -0500

    adicion de enlace para acceso rapido a la aplicacion

--- backend/src/bin/index.js
+++ backend/src/bin/index.js
@@ -2,5 +2,5 @@ import app from '../app';
 
 // Listener Server
 app.listen(app.get('port'), () => {
-    console.log(`Server runnig on port ${app.get('port')}`)
+    console.log(`Server runnig on port ${app.get('port')}\nhttp://localhost:${app.get('port')}`)
 });
